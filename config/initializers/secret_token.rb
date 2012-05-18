2209b8dfe273a0d1b9938bb2ddad4315898a3ca9
Uko <uko@unikernel.net>
2012-05-18 21:31:21 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnAppTask::Application.config.secret_token = '4ab9ccf6cd16d669df279caa88df8dd31c2d4af9ac974a5c6136a598af5b887f39eaf43a71575a6d6da5abc2606b2f1b4c237b43b33ff6158d9ae62cff847902'
