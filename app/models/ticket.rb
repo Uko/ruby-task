846b38730d2b2c206b61256fa87609c92e9016c2
Uko <uko@unikernel.net>
2012-05-18 22:57:29 +0300

    Added validation and acceptable values to ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,16 @@
 class Ticket < ActiveRecord::Base
   belongs_to :employee
   attr_accessible :customer_mail, :customer_name, :department, :status
+
+	validates :customer_mail, :customer_name, :department, :presence => true
+	validates :department, :inclusion => { :in => self.all_departments}
+	validates :status, :inclusion => { :in => self.all_statuses}
+	
+	def self.all_departments
+		%w(billing documentation technical)
+	end
+	
+	def self.all_statuses
+		['Waiting for Staff Response', 'Waiting for Customer', 'On Hold', 'Cancelled', 'Completed']
+	end
 end
