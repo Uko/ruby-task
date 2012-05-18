28d43945d0b75fc46276dd04be14decb7c24bf1b
Uko <uko@unikernel.net>
2012-05-19 00:52:28 +0300

    Completed 'new ticket' page Added description to ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,8 @@
 class Ticket < ActiveRecord::Base
   belongs_to :employee
-  attr_accessible :customer_mail, :customer_name, :department, :status
+  attr_accessible :customer_mail, :customer_name, :department, :description, :status
+
+	after_initialize :default_values
 
 	def self.all_departments()
 		%w(billing documentation technical)
@@ -11,7 +13,13 @@ class Ticket < ActiveRecord::Base
 	end
 
 	validates :customer_mail, :customer_name, :department, :presence => true
+	validates :customer_mail, :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
 	validates :department, :inclusion => { :in => Ticket.all_departments()}
 	validates :status, :inclusion => { :in => Ticket.all_statuses}
+	
+	private
+    def default_values
+      self.status ||= 'Waiting for Staff Response'
+    end
 	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120518192401) do
+ActiveRecord::Schema.define(:version => 20120518212227) do
 
   create_table "employees", :force => true do |t|
     t.string   "login"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20120518192401) do
     t.integer  "employee_id"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.text     "description"
   end
 
   add_index "tickets", ["employee_id"], :name => "index_tickets_on_employee_id"
