95b0fc4420d5e703dffc5f6576a1cdd09a376b3a
Uko <uko@unikernel.net>
2012-05-20 23:09:12 +0300

    Added replies to Tasks

--- app/controllers/backend/tickets_controller.rb
+++ app/controllers/backend/tickets_controller.rb
@@ -14,20 +14,10 @@ class Backend::TicketsController < Backend::ApplicationController
   # GET /tickets/1.json
   def show
     @ticket = Ticket.find(params[:id])
-
+		@reply = @ticket.replies.build
+		
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @ticket }
-    end
-  end
-
-  # GET /tickets/new
-  # GET /tickets/new.json
-  def new
-    @ticket = Ticket.new
-
-    respond_to do |format|
-      format.html # new.html.erb
       format.json { render json: @ticket }
     end
   end
@@ -37,34 +27,29 @@ class Backend::TicketsController < Backend::ApplicationController
     @ticket = Ticket.find(params[:id])
   end
 
-  # POST /tickets
-  # POST /tickets.json
-  def create
-    @ticket = Ticket.new(params[:ticket])
-
-    respond_to do |format|
-      if @ticket.save
-				CustomerSupport.notification_email(@ticket).deliver
-        format.html { redirect_to @ticket, notice: 'Ticket was successfully created.' }
-        format.json { render json: @ticket, status: :created, location: @ticket }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @ticket.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # PUT /tickets/1
   # PUT /tickets/1.json
   def update
     @ticket = Ticket.find(params[:id])
-
+		changes = ""
+		unless params[:ticket][:employee_id].to_i == @ticket.employee.id
+			changes += "> Responsible changed from #{@ticket.employee.login} to #{Employee.find(params[:ticket][:employee_id]).login}"
+			@ticket.employee = Employee.find(params[:ticket][:employee_id])
+		end
+		unless params[:ticket][:status] == @ticket.status
+			changes += $/ + "> Status changed from \"#{@ticket.status}\" to \"#{params[:ticket][:status]}\""
+			@ticket.status = params[:ticket][:status]
+		end
+		@reply = @ticket.replies.build(params[:reply])
+		@reply.meta = changes
     respond_to do |format|
-      if @ticket.update_attributes(params[:ticket])
-        format.html { redirect_to @ticket, notice: 'Ticket was successfully updated.' }
+      if @ticket.valid? && @reply.valid?
+				@ticket.save
+				@reply.save
+        format.html { redirect_to backend_ticket_path(@ticket), notice: 'Ticket was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
+        format.html { render action: "show" }
         format.json { render json: @ticket.errors, status: :unprocessable_entity }
       end
     end
