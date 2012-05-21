405687e31024a50f96707b04de353022a99e7804
Uko <uko@unikernel.net>
2012-05-21 07:04:51 +0300

    Removed status inclusive validation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -18,7 +18,6 @@ class Ticket < ActiveRecord::Base
 	validates :customer_mail, :customer_name, :department, :subject, :status, :presence => true
 	validates :customer_mail, :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
 	validates :department, :inclusion => { :in => Ticket.all_departments()}
-	validates :status, :inclusion => { :in => Ticket.all_statuses}
 	
 	private
     def default_values
